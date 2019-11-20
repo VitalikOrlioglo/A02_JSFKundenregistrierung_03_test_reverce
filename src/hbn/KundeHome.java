41981169c42086d04306b5ff9791aa594452843d
Vitali Orlioglo <vitalik.orlioglo@gmail.com>
2019-11-20 16:29:30 +0100

    Hibernate Reverse Engineering

---
+++ src/hbn/KundeHome.java
@@ -0,0 +1,116 @@
+package hbn;
+// Generated 20.11.2019 12:10:54 by Hibernate Tools 5.4.7.Final
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Kunde.
+ * @see hbn.Kunde
+ * @author Hibernate Tools
+ */
+public class KundeHome {
+
+	private static final Logger logger = Logger.getLogger(KundeHome.class.getName());
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext().lookup("SessionFactory");
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Kunde transientInstance) {
+		logger.log(Level.INFO, "persisting Kunde instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			logger.log(Level.INFO, "persist successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Kunde instance) {
+		logger.log(Level.INFO, "attaching dirty Kunde instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			logger.log(Level.INFO, "attach successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Kunde instance) {
+		logger.log(Level.INFO, "attaching clean Kunde instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			logger.log(Level.INFO, "attach successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Kunde persistentInstance) {
+		logger.log(Level.INFO, "deleting Kunde instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			logger.log(Level.INFO, "delete successful");
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "delete failed", re);
+			throw re;
+		}
+	}
+
+	public Kunde merge(Kunde detachedInstance) {
+		logger.log(Level.INFO, "merging Kunde instance");
+		try {
+			Kunde result = (Kunde) sessionFactory.getCurrentSession().merge(detachedInstance);
+			logger.log(Level.INFO, "merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "merge failed", re);
+			throw re;
+		}
+	}
+
+	public Kunde findById(java.lang.Integer id) {
+		logger.log(Level.INFO, "getting Kunde instance with id: " + id);
+		try {
+			Kunde instance = (Kunde) sessionFactory.getCurrentSession().get("hbn.Kunde", id);
+			if (instance == null) {
+				logger.log(Level.INFO, "get successful, no instance found");
+			} else {
+				logger.log(Level.INFO, "get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Kunde instance) {
+		logger.log(Level.INFO, "finding Kunde instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria("hbn.Kunde").add(Example.create(instance))
+					.list();
+			logger.log(Level.INFO, "find by example successful, result size: " + results.size());
+			return results;
+		} catch (RuntimeException re) {
+			logger.log(Level.SEVERE, "find by example failed", re);
+			throw re;
+		}
+	}
+}
